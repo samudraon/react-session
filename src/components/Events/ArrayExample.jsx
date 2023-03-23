35534e37b80e20e1d4f5b9db5459388c4f584dd7
samudraon <samudraon@gmailcom>
2023-03-23 23:23:57 +0600

    map and filter added

--- src/components/Events/ArrayExample.jsx
+++ src/components/Events/ArrayExample.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { guns } from '../../../test';
 
 const ArrayExample = () => {
+    const [data, setData] = useState(guns)
+    // console.log(data);
+    const handleRemove = () => {
+        setData([])
+    }
+
+    const handleSingleRemove = (id) => {
+        /* option - 1 */
+        // const filterData = data.filter(d => d.id !== id);
+        
+        /* option - 2 */
+        setData(data.filter(d => d.id !== id));
+    };
+
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className='text-3xl text-center font-bold my-4'>
+            {
+                data && data.map(singleData => (
+                    <div>
+                        <p>{singleData.name}</p>
+                        <button onClick={()=>handleSingleRemove(singleData.id)} className='btn btn-error mt-4'>Remove</button>
+                    </div>
+                ))
+            }
+            <div>
+                <button onClick={handleRemove} className='btn btn-active mt-4'>Remove All</button>
+            </div>
         </div>
+
     );
 };
 
